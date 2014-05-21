699931f775afde028e1ac9e0d582cdf509c2d60e
kalfasyan <kalfasyan@gmail.com>
2014-05-22 00:15:39 +0200

    uploading latest files
    yes.

--- stuff/testing programming/hw2sysbio.m
+++ stuff/testing programming/hw2sysbio.m
@@ -1,11 +1,11 @@
 %% stuff
 inits='x(0)=1,y(0)=0';
 t=linspace(0,5);
-[x,y]=dsolve('Dx=productionX(0.5,1)-x','Dy=productionX(0.8,1)-y',inits)
+[x,y]=dsolve('Dx=productionX(0.5,1)-x','Dy=productionX(0.5,1)-y',inits)
 xx=eval(vectorize(x));
 yy=eval(vectorize(y));
 
-
+figure;
 plot(t,xx,t,yy)
 xlabel('time')
 ylabel('Production of X,Y')
--- stuff/testing programming/productionX.m
+++ stuff/testing programming/productionX.m
@@ -2,8 +2,7 @@ function [ rateX ] = productionX( Y, Kyx )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 beta = 1; % maximum rate
-n = 1 % Hill coefficient
+n = 9999999999 % Hill coefficient
 rateX = beta * Kyx^n / (Y + Kyx)^n;
-
 
 return
